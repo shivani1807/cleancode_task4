d7ee1fc65871352f99eb7f3ffe68aaee9801bbe9
shivani1807 <shivanibommidi96@gmail.com>
2020-07-20 14:12:52 +0530

    1st commit

---
+++ src/test/java/simple_interest/Jtest_intrst.java
@@ -0,0 +1,14 @@
+package simple_interest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Jtest_intrst {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
